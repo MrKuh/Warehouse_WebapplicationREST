d9084b5208120fdf68c15746587a2fcbf0269f9f
Bensi <renbea18@htl-kaindorf.at>
2022-03-10 15:21:38 +0100

    Rest Api standard methods

---
+++ src/main/java/at/htlkaindorf/warehouse_webapplicationrest/WebApplication.java
@@ -0,0 +1,17 @@
+package at.htlkaindorf.warehouse_webapplicationrest;
+
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@WebListener
+@ApplicationPath("/api")
+public class WebApplication extends Application implements ServletContextListener {
+
+    /*@Override
+    public void contextInitialized(ServletContextEvent sce){
+        CustomerDB.getInstance().setXmlFile(sce.getServletContext().getRealPath("/xml/customerList.xml"));
+        CustomerDB.getInstance().loadXMLData();
+    }*/
+}
---
+++ src/main/java/at/htlkaindorf/warehouse_webapplicationrest/WebDataBase.java
@@ -0,0 +1,15 @@
+package at.htlkaindorf.warehouse_webapplicationrest;
+
+import jakarta.xml.bind.JAXB;
+
+import javax.management.openmbean.KeyAlreadyExistsException;
+import java.io.File;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class WebDataBase {
+
+    //private static CustomerDB theInstance;
+
+}
