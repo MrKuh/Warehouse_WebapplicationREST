23945bac8ad9f716fc216f4e6e040d03d8c87b09
MrKuh <michi.wajand@gmail.com>
2022-03-10 14:55:53 +0100

    arrow bug fixed

--- src/main/webapp/js/arrow.js
+++ src/main/webapp/js/arrow.js
@@ -1,20 +1,17 @@
-const cnv = document.getElementById("myCanvas");
+const cnv = document.getElementById("active");
+const cnv2 = document.getElementById("next");
 const jg = new jsGraphics(cnv);
+const jg2 = new jsGraphics(cnv2);
+let help;
 
-function myDrawFunction(source, target, check){
+
+function drawActive(source, target){
     const coords = getCoords(source, target);
 
-    if (check){
-        help = 0.8
+    help = 0.8
 
-        jg.setColor("#0000ff"); // blau
-        jg.setStroke(6);
-    }else {
-        help = 1.2
-
-        jg.setColor("#808080"); // grau
-        jg.setStroke(6);
-    }
+    jg.setColor("#0000ff"); // blau
+    jg.setStroke(6);
 
     jg.drawLine(coords[0] + coords[4] / 2, coords[1] + coords[5], coords[0] + coords[4] / 2,
         (coords[1] + coords[5] * help + coords[3]) / 2);
@@ -32,6 +29,31 @@ function myDrawFunction(source, target, check){
     jg.paint();
 }
 
+
+function drawNext(source, target){
+    const coords = getCoords(source, target);
+
+    help = 1.2
+
+    jg2.setColor("#808080"); // grau
+    jg2.setStroke(6);
+
+    jg2.drawLine(coords[0] + coords[4] / 2, coords[1] + coords[5], coords[0] + coords[4] / 2,
+        (coords[1] + coords[5] * help + coords[3]) / 2);
+
+    jg2.drawLine(coords[0] + coords[4] / 2, (coords[1] + coords[5] * help + coords[3]) / 2, coords[2] + coords[6] / 2,
+        (coords[1] + coords[5] * help + coords[3]) / 2);
+
+    jg2.drawLine(coords[2] + coords[6] / 2, (coords[1] + coords[5] * help + coords[3]) / 2,
+        coords[2] + coords[6] / 2, coords[3]);
+
+    jg2.drawPolyline([(coords[2] + (coords[6] / 2) * 0.9), coords[2] + coords[6] / 2, (coords[2] + (coords[6] / 2) * 1.1)],
+        [(coords[3] + (coords[1] + coords[5] + coords[3]) / 2 * 1.1) / 2, coords[3],
+            (coords[3] + (coords[1] + coords[5] + coords[3]) / 2 * 1.1) / 2]);
+
+    jg2.paint();
+}
+
 function getCoords(source, target) {
     const coords = [source.getBoundingClientRect().x, source.getBoundingClientRect().y,
         target.getBoundingClientRect().x, target.getBoundingClientRect().y,
@@ -44,4 +66,5 @@ function getCoords(source, target) {
 
 function deleteLine(){
     jg.clear();
+    jg2.clear();
 }
